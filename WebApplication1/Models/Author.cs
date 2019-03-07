94012ba341ae0bb92da9ab27b6285ee42221c6fc
pzed220 <pzed220@gmail.com>
2019-03-07 23:05:21 +0300

    start

---
+++ WebApplication1/Models/Author.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models {
+    public class Author {
+        public string FirstName { get; set; }//req <=20
+        public string LastName { get; set; }//req <= 20
+    }
+}
---
+++ WebApplication1/Models/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models {
+    public class Book {
+        public string Caption { get; set; }//req <=30
+        public IList<Author> Authors => new List<Author>();//req >=1
+        public int Pages { get; set; }//req >0; <=1000
+        public int Year { get; set; }//opt <=1800
+        public string Publisher { get; set; }//opt <=30
+        public string ISBN { get; set; }//req, valid mask, valid net
+    }
+}
