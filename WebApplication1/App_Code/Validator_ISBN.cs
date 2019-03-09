66854eb80467c9ba45c1a8334aa571b875cf46e7
pzed220 <pzed220@gmail.com>
2019-03-09 23:58:54 +0300

    start

--- WebApplication1/App_Code/Validator_ISBN.cs
+++ WebApplication1/App_Code/Validator_ISBN.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using ProductCodeValidator;
 
 namespace WebApplication1.App_Code {
     public static class Validator_ISBN {
         public static ValidationResult Check(object value, ValidationContext context) {
+            string isbn = value.ToString();
+            if(IsbnValidator.EanIsIsbn(isbn))
+                return ValidationResult.Success;
+            
+            //ProductCodeValidator.IsbnValidator.
             return ValidationResult.Success;
         }
     }
